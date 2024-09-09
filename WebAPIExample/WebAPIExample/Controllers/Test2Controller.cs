2f203130961e0e65de8e13c8bf4e4fe496138416
JTLin-nzxt <jt.lin@nzxt.com>
2024-09-09 13:17:43 +0800

    init/ init the project with the demo example

---
+++ WebAPIExample/WebAPIExample/Controllers/Test2Controller.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPIExample.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class Test2Controller : ControllerBase
+    {
+        private readonly ILogger<Test2Controller> _logger;
+
+        public Test2Controller(ILogger<Test2Controller> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public string Get()
+        {
+            return "Hello";
+        }
+    }
+}
