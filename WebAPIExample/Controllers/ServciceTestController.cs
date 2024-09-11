111519809ede5bf39073c637f3d83b3078ada2e3
JTLin-nzxt <jt.lin@nzxt.com>
2024-09-11 14:06:32 +0800

    patch/new controller ServiceTest

---
+++ WebAPIExample/Controllers/ServciceTestController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPIExample.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class ServiceTestController : ControllerBase
+    {
+        private readonly ILogger<ServiceTestController> _logger;
+
+        public ServiceTestController(ILogger<ServiceTestController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public string Get()
+        {
+            return "ServiceTest Triggered";
+        }
+    }
+}
